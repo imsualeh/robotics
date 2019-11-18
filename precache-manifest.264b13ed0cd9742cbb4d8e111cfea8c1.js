ea4ec40fd45ef2b4b3569a15ab60bd87eecdc3de
imsualeh <mdsknj@gmail.com>
2019-11-18 12:02:42 +0530

    Updates

---
+++ precache-manifest.264b13ed0cd9742cbb4d8e111cfea8c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b0dfa6f7e310997aa1aaa46600b38f",
+    "url": "/robotics/index.html"
+  },
+  {
+    "revision": "f616361ce9e732c22317",
+    "url": "/robotics/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "478b4ba70bdb62d0b9c9",
+    "url": "/robotics/static/css/main.831c7344.chunk.css"
+  },
+  {
+    "revision": "f616361ce9e732c22317",
+    "url": "/robotics/static/js/2.ffca6f0d.chunk.js"
+  },
+  {
+    "revision": "478b4ba70bdb62d0b9c9",
+    "url": "/robotics/static/js/main.ab89d5e8.chunk.js"
+  },
+  {
+    "revision": "d34831b3d4a15e716746",
+    "url": "/robotics/static/js/runtime-main.60c64745.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotics/static/media/SEGA.12a3db8b.woff"
+  }
+]);
